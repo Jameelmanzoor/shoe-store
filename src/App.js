e582ee783f88233d82465b0fb47b5f94e4cc46fd
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-10-05 10:00:06 +0500

    add routes to navigate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import ProductDetails from './components/ProductDetails';
+import Products from './components/Products';
 
 
 function App() {
   return (
     <div>
-      Abc
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='products' element={<Products />}>
+          <Route path=':productId' element={<ProductDetails />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
