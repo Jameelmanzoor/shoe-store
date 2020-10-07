09d8f9cd6f3574e16a5237275f8269def9e7bc04
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-10-07 21:20:14 +0500

    Add two more links in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core';
+import { IconButton, makeStyles, Tooltip } from '@material-ui/core';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import logo from '../resources/logo-white.svg';
 
 const useStyles = makeStyles(() => ({
@@ -22,8 +24,19 @@ const useStyles = makeStyles(() => ({
   linkContainer: {
     flexGrow: 1,
     display: 'flex',
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  iconColor: {
+    color: 'white'
+  },
+  boxCol: {
+    '&:hover': {
+      boxShadow: '#ffffff'
+    }
+
   }
+
 }))
 
 const Header = () => {
@@ -35,11 +48,26 @@ const Header = () => {
           <img src={logo} alt="logo" className={classes.logo} />
           <div className={classes.linkContainer}>
             <Typography variant='h6'>
-              <Link to='/' className={classes.link}>Home</Link>
+              <Link to='/' className={`${classes.link} ${classes.boxCol}`}>Home</Link>
             </Typography>
             <Typography variant='h6'>
-              <Link to='products' className={classes.link}>Products</Link>
+              <Link to='products' className={`${classes.link} ${classes.boxCol}`}>
+                Products</Link>
             </Typography>
+            <a target='_blank' href='https://github.com/Jameelmanzoor' rel='noopener'>
+              <Tooltip title='GitHub code repository'>
+                <IconButton aria-label='github code repository' className={classes.iconColor}>
+                  <GitHubIcon />
+                </IconButton>
+              </Tooltip>
+            </a>
+            <Link to='cart' className={classes.link}>
+              <Tooltip title='Shoping cart'>
+                <IconButton aria-label='add to shopping cart' className={classes.iconColor}>
+                  <ShoppingCartIcon />
+                </IconButton>
+              </Tooltip>
+            </Link>
           </div>
         </Toolbar>
       </AppBar>
