86df5d84b6283aa84b1a1906bc40dc80473c94ae
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-10-11 21:56:35 +0500

    automate responsive font size according to screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Grid, Button, makeStyles, Typography } from '@material-ui/core';
+import { Grid, Button, makeStyles, Typography, Box } from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import homeImage from '../resources/home-page.png';
 import { useNavigate } from 'react-router-dom';
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,28 +17,49 @@ const useStyles = makeStyles((theme) => ({
   },
   img: {
     [theme.breakpoints.down('sm')]: {
-      width: 340,
+      width: '95vw',
+      marginTop: 40,
+    },
+  },
+  heading: {
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+      marginTop: 30
     }
   },
-
+  btn: {
+    [theme.breakpoints.down('sm')]: {
+      margin: '0 auto',
+      display: 'flex',
+      alignContent: 'center',
+      marginTop: 20
+    }
+  }
 }))
 
 const Home = () => {
   const classes = useStyles();
   const navigate = useNavigate();
   return (
-
     <Grid container direction='row-reverse' alignItems='center' className={classes.root}>
       <Grid item xs={12} sm={12} md={7}>
         <img src={homeImage} alt='Shoe on home page' className={classes.img} />
       </Grid>
       <Grid item xs={12} sm={12} md={5}>
-        <Typography variant='h2'>Welcome, Find Your Dream Shoes Here</Typography>
-        <Button
-          variant='contained'
-          color='primary'
-          endIcon={<ArrowForwardIcon />}
-          onClick={() => navigate('products')}>Buy Now</Button>
+
+        <Typography variant='h2' className={classes.heading}>
+          Welcome, Find Your Dream Shoes Here
+          </Typography>
+        <Box >
+          <Button
+            variant='contained'
+            color='primary'
+            endIcon={<ArrowForwardIcon />}
+            onClick={() => navigate('products')}
+            className={classes.btn}>
+            Buy Now
+            </Button>
+        </Box>
       </Grid>
     </Grid>
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalContextProvider } from './context/GlobalContext';
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import './App.css';
 
-
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 ReactDOM.render(
   <GlobalContextProvider>
     <Router>
-      <App />
+      <ThemeProvider theme={theme}  >
+        <App />
+      </ThemeProvider>
     </Router>
   </GlobalContextProvider>,
   document.getElementById('root')
